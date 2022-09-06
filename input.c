7320039dd5fd3811ac0c0e880eeb46a3fad86bcf
omochi8h <omochi8h@gmail.com>
2022-09-06 17:07:58 +0900

    0906

--- input.c
+++ input.c
@@ -4,11 +4,10 @@ int n;
 printf("点数を入力してください\n");
 scanf("%d" ,&n);
 if(n>=90)printf("S\n");
-else if(n>=80)printf("A\n");
-else if(n>=70)printf("B\n");
-
-	else if(n>=60)printf("C\n");
-	else printf("F\n");
+if(n>=80 & n<90)printf("A\n");
+if(n>=70 & n<80)printf("B\n");
+if(n>=60 & n<70)printf("C\n");
+else printf("F\n");
 
 return 0;
 }
